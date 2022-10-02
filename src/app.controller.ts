2ca3a7a86e9a9b63aa6320b30be1f71f584e7e5a
pandracoon <whworjs7946@naver.com>
2022-10-02 14:23:43 +0900

    swagger init

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
   @Get()
+  @ApiOperation({ summary: 'Hello~' })
+  @ApiResponse({ status: 200, description: 'Hi~', type: String })
   getHello(): string {
     return this.appService.getHello();
   }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,15 @@ async function bootstrap() {
       skipUndefinedProperties: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('server.kuaaa.net')
+    .setDescription('KUAAA API description')
+    .setVersion('1.0')
+    .addTag('KUAAA')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(3000);
 }
