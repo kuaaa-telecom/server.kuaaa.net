1c7302d1e99baa840682db151199028d9c033e02
pandracoon <whworjs7946@naver.com>
2022-10-02 20:53:40 +0900

    env validation setting

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Kuaaa!';
+    return process.env.DATABASE_URL;
   }
 }
