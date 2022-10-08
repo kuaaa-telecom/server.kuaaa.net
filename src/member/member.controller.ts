b65a9682f53213d1baebb870de79a36d56f094c3
pandracoon <whworjs7946@naver.com>
2022-10-09 00:33:18 +0900

    controller, service setUp

--- src/member/member.controller.ts
+++ src/member/member.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { MemberService } from './member.service';
+import {
+  ApiNoContentResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
+import { CreateResultDto } from './dto/create-result.dto';
+import { CreateMembersPayload } from './payload/create-members.payload';
+import { UpdateMemberPayload } from './payload/update-member.payload';
 
-@Controller('member')
-export class MemberController {}
+@ApiTags('Member API')
+@Controller('api/members')
+export class MemberController {
+  constructor(private readonly memberService: MemberService) {}
+
+  @Post()
+  @ApiOperation({ summary: '회원 명부에 여러 회원들을 등록합니다.' })
+  @ApiOkResponse({ type: CreateResultDto })
+  async createMembers(
+    @Body() payload: CreateMembersPayload,
+  ): Promise<CreateResultDto> {
+    return this.memberService.createMembers(payload);
+  }
+
+  @Patch(':id')
+  @ApiOperation({ summary: '해당 회원의 정보를 수정합니다.' })
+  @ApiNoContentResponse()
+  async updateMember(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() payload: UpdateMemberPayload,
+  ): Promise<void> {
+    await this.memberService.updateMember(id, payload);
+  }
+
+  @Delete(':id')
+  @ApiOperation({ summary: '해당 회원의 정보를 삭제합니다.' })
+  @ApiNoContentResponse()
+  async deleteMember(@Param('id', ParseUUIDPipe) id: string): Promise<void> {
+    await this.memberService.deleteMember(id);
+  }
+}
--- src/member/member.service.ts
+++ src/member/member.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { CreateMembersPayload } from './payload/create-members.payload';
+import { CreateResultDto } from './dto/create-result.dto';
+import { UpdateMemberPayload } from './payload/update-member.payload';
 
 @Injectable()
-export class MemberService {}
+export class MemberService {
+  async createMembers(payload: CreateMembersPayload): Promise<CreateResultDto> {
+    return {} as any;
+  }
+
+  async updateMember(id: string, payload: UpdateMemberPayload): Promise<void> {
+    return;
+  }
+
+  async deleteMember(id: string): Promise<void> {
+    return;
+  }
+}
