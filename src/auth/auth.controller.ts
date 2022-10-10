58797f39d66be653a3a35f5775e7e62fa01dfd86
pandracoon <whworjs7946@naver.com>
2022-10-10 20:15:52 +0900

    add method to controller & service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import {
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
+import { AuthService } from './auth.service';
+import { TokenDto } from './common/dto/token.dto';
+import { LoginPayload } from './common/payload/login.payload';
+import { SignupPayload } from './common/payload/signup.payload';
 
-@Controller('auth')
-export class AuthController {}
+@ApiTags('Auth API')
+@Controller('api/auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('sign-up')
+  @ApiOperation({ summary: '계정을 생성합니다.' })
+  @ApiCreatedResponse({ type: TokenDto })
+  async signUp(@Body() payload: SignupPayload): Promise<TokenDto> {
+    return this.authService.signUp(payload);
+  }
+
+  @Post('login')
+  @ApiOperation({ summary: '로그인합니다.' })
+  @ApiOkResponse({ type: TokenDto })
+  async login(@Body() payload: LoginPayload): Promise<TokenDto> {
+    return this.authService.login(payload);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { SignupPayload } from './common/payload/signup.payload';
+import { TokenDto } from './common/dto/token.dto';
+import { LoginPayload } from './common/payload/login.payload';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor() {}
+
+  async signUp(payload: SignupPayload): Promise<TokenDto> {
+    return {} as any;
+  }
+
+  async login(payload: LoginPayload): Promise<TokenDto> {
+    return {} as any;
+  }
+}
