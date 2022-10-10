622507eaf2755ca938535def948e2347890c7fb2
pandracoon <whworjs7946@naver.com>
2022-10-10 23:11:46 +0900

    set test bearerAuth

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,7 @@ async function bootstrap() {
     .setTitle('server.kuaaa.net')
     .setDescription('KUAAA API description')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
