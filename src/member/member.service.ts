2d146b8db11513abc50a7ffd59979f933054ad7b
worjs <whworjs777@gmail.com>
2022-10-13 20:24:50 +0900

    add getMember method to service & controller

--- src/member/member.service.ts
+++ src/member/member.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { MemberFactory } from './member/member.factory';
+import { Account } from '../auth/account/account.entity';
+import { MemberInfoDto } from './common/dto/member-info.dto';
 
 @Injectable()
 export class MemberService {
   constructor(private readonly memberFactory: MemberFactory) {}
+
+  async getMember(account: Account): Promise<MemberInfoDto> {
+    const member = await this.memberFactory.getMemberById(account.memberId);
+    return {} as any;
+  }
 }
